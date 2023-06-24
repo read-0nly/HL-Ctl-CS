e38a70fbe45b6495d9db448183fe8b5aef940196
obsol <33932119+read-0nly@users.noreply.github.com>
2023-06-24 19:26:40 -0400

    I saw the light!
    Lo, and god said QHM-0274
    
    Bit  of back and forth and relearning lessons i wouldn't have had to learn  if I had just read the damn article but here we are, enumeration catches the light.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace BleConsoleDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var watcher = new BluetoothLEAdvertisementWatcher();
+            watcher = new BluetoothLEAdvertisementWatcher()
+            {
+                ScanningMode = BluetoothLEScanningMode.Passive
+            };
+
+            watcher.Received += Watcher_Received;
+            watcher.Start();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static async void Watcher_Received(
+            BluetoothLEAdvertisementWatcher sender,
+            BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+            if (device != null)
+            {
+                Console.WriteLine(device.DeviceInformation.Name);
+
+                /*
+                 * This passes tiny red line check - still gotta figure out everything between advertisement and this.
+                Windows.Storage.Streams.IBuffer buff = Windows.Security.Cryptography.CryptographicBuffer.ConvertStringToBinary(
+    "What fools these mortals be", Windows.Security.Cryptography.BinaryStringEncoding.Utf8);
+                await device.GetGattService(new Guid()).GetCharacteristics(new Guid())[0].WriteValueAsync(buff);
+
+                */
+            }
+        }
+    }
+}
