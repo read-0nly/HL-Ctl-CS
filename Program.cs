386de40e76ba6e9e2ca4d9ed3d290a4d2628575c
obsol <33932119+read-0nly@users.noreply.github.com>
2023-06-24 20:07:10 -0400

    Found TX/RX
    HArd-bind by mac address is bad but hey, we're so, so close. BLE stuff seems to be mostly figured out enough to use, just gotta get the protocol implemented now I  guess

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
 
 namespace BleConsoleDemo
 {
@@ -29,8 +34,40 @@ namespace BleConsoleDemo
             var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
             if (device != null)
             {
-                Console.WriteLine(device.DeviceInformation.Name);
+                if(device.DeviceInformation!=null && device.DeviceInformation.Name.Contains("QHM"))
+                {
+                    if(string.Format("{0:X}", device.BluetoothAddress)== "720513000274")
+                    {
+                        await device.RequestAccessAsync();
+                        await device.GetGattServicesAsync();
+                        foreach(GattDeviceService service in device.GattServices)
+                        {
+                            Console.WriteLine("Found serv " + service.Uuid);
+                            await service.RequestAccessAsync();
+                            GattCharacteristicsResult gcr = await service.GetCharacteristicsAsync();
+                            foreach(var x in gcr.Characteristics)
+                            {
+                                GattCharacteristicProperties properties = x.CharacteristicProperties;
+                                if (properties.HasFlag(GattCharacteristicProperties.WriteWithoutResponse) && properties.HasFlag(GattCharacteristicProperties.Write))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Found TX");
+                                    Console.ResetColor();
+                                }
+                                else if (service.Uuid.ToString() == "00001800-0000-1000-8000-00805f9b34fb")
+                                {
 
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Found RX");
+                                    Console.ResetColor();
+                                }
+                                Console.WriteLine(properties.ToString());
+                            }
+
+                        }
+                    }
+
+                }
                 /*
                  * This passes tiny red line check - still gotta figure out everything between advertisement and this.
                 Windows.Storage.Streams.IBuffer buff = Windows.Security.Cryptography.CryptographicBuffer.ConvertStringToBinary(
